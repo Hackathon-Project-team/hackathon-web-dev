80c91686f346918b766433db03ea7f1c8eadad59
Abhishek Sharma <abhishekalien09@gmail.com>
2021-06-26 13:25:06 +0530

    Added css

--- src/components/Form.js
+++ src/components/Form.js
@@ -1,27 +1,32 @@
 import React from "react";
 
 
-export default function Form(props){
+export default function Form(props) {
+    let admin = {
+    position: "absolute" , 
+    top: "50%" , 
+    left: "50%" , 
+    margin: "10px" , 
+    padding: "40px" , 
+    transform: "translate(-50% , -50%)"
+    }
     return (
-        <div>
+        <div className = "container text-center bg-dark text-light" style = {admin}>
             <form>
                 <div className="mb-3">
-                    <label for="exampleInputEmail1" className="form-label">Email address</label>
-                    <input type="email" className="form-control" id="exampleInputEmail1" aria-describedby="emailHelp"/>
-                        <div id="emailHelp" className="form-text">We'll never share your email with anyone else.</div>
-  </div>
-                    <div className="mb-3">
-                        <label for="exampleInputPassword1" className="form-label">Password</label>
-                        <input type="password" className="form-control" id="exampleInputPassword1"/>
-  </div>
-                        <div className="mb-3 form-check">
-                            <input type="checkbox" className="form-check-input" id="exampleCheck1"/>
-                                <label className="form-check-label" for="exampleCheck1">Check me out</label>
-  </div>
-                            <button type="submit" className="btn btn-primary">Submit</button>
-</form>
-</div>
-                        );
+                    <label for="exampleInputEmail1" className="form-label">Admin Username</label>
+                    <input type="email" className="form-control" id="exampleInputEmail1" aria-describedby="emailHelp" />
+
+                </div>
+                <div className="mb-3">
+                    <label for="exampleInputPassword1" className="form-label">Password</label>
+                    <input type="password" className="form-control" id="exampleInputPassword1" />
+                </div>
+
+                <button type="submit" className="btn btn-primary">Submit</button>
+            </form>
+        </div>
+    );
 
 }
 
